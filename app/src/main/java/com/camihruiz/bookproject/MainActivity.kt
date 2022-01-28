f58741d829a12e1ec5762dd6083df5f857cea4e2
Camilo Hernández Ruiz <camihruiz24@gmail.com>
2022-01-27 22:41:28 -0500

    Añadido el mainBinding y el Toast.

--- app/src/main/java/com/camihruiz/bookproject/MainActivity.kt
+++ app/src/main/java/com/camihruiz/bookproject/MainActivity.kt
@@ -2,22 +2,38 @@ package com.camihruiz.bookproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
+//import android.widget.Button
+//import android.widget.EditText
+//import android.widget.TextView
+import com.camihruiz.bookproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var mainBinding : ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
 
-        val bookNameEditText : EditText = findViewById(R.id.book_name_edit_text)
-        val saveButton : Button = findViewById(R.id.save_button)
-        val infoTextView : TextView = findViewById(R.id.info_text_view)
-
-        saveButton.setOnClickListener{
-            val nameBook : String = bookNameEditText.text.toString()
-            infoTextView.text = nameBook
+        // val bookNameEditText : EditText = findViewById(R.id.book_name_edit_text)
+        
+        with (mainBinding) {
+            saveButton.setOnClickListener {
+        
+                if (bookNameEditText.text.isEmpty() || authorNameEditText.text.isEmpty() || pagesEditText.text?.isEmpty() == true) {
+                    Toast.makeText(applicationContext,"Debe digital nombre, autor y número de páginas",Toast.LENGTH_SHORT).show()
+                } else {
+                    val nameBook: String = bookNameEditText.text.toString()
+                    val author = authorNameEditText.text.toString()
+                    val pages = pagesEditText.text.toString().toInt()
+                    val abstract = abstractInputText.text.toString()
+    
+                    infoTextView.text =
+                        "Name: " + nameBook + "Author: " + author + "Pages: " + pages + " Abstract: " + abstract
+                }
+            }
         }
     }
 }
